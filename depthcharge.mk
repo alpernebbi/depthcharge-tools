871acf63a955692a40e4ae6ae6b06401a33f7b49
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2019-10-21 20:59:17 +0300

    Only create a single image in the working dir
    I was using the earlier versions to mass-build and test different
    configurations, which is no longer necessary since now I know how to
    make it work.
    
    Signed-off-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>

--- depthcharge.mk
+++ depthcharge.mk
@@ -1,23 +1,22 @@
 #! /usr/bin/make -f
 
-dirs = $(wildcard */)
-all: $(addsuffix kernel.img, $(dirs))
+all: kernel.img
 
 keys := kernel.keyblock kernel_data_key.vbprivk
 
-%.img: %.itb %.args bootloader.bin $(keys)
+kernel.img: kernel.itb kernel.args bootloader.bin $(keys)
 	vbutil_kernel \
 		--version 1 \
 		--arch aarch64 \
 		--pack $@ \
-		--vmlinuz $*.itb \
-		--config $*.args \
+		--vmlinuz kernel.itb \
+		--config kernel.args \
 		--bootloader bootloader.bin \
 		--keyblock kernel.keyblock \
 		--signprivate kernel_data_key.vbprivk
 	test "$$(stat -c '%s' $@)" -lt 33554432
 
-%/kernel.itb: %/vmlinuz.lz4 %/initrd.img %/rk3399-gru-kevin.dtb
+kernel.itb: vmlinuz.lz4 initrd.img rk3399-gru-kevin.dtb
 	mkimage \
 		-D "-I dts -O dtb -p 2048" \
 		-f auto \
@@ -26,24 +25,24 @@ keys := kernel.keyblock kernel_data_key.vbprivk
 		-T kernel \
 		-C lz4 \
 		-a 0 \
-		-d $*/vmlinuz.lz4 \
-		-i $*/initrd.img \
-		-b $*/rk3399-gru-kevin.dtb \
+		-d vmlinuz.lz4 \
+		-i initrd.img \
+		-b rk3399-gru-kevin.dtb \
 		$@
 
-%/kernel.args: initrd.args
+kernel.args: initrd.args
 	@echo 'root=PARTUUID=4f7a82a0-1e9a-47fd-83b5-73847350f068' > $@
 	@echo 'console=tty1' >> $@
 	@echo 'rootwait' >> $@
 	@echo 'rw' >> $@
 
-%.lz4: %
+vmlinuz.lz4: vmlinuz
 	lz4 -12 $< $@
 
 bootloader.bin:
 	dd if=/dev/zero of=$@ count=1
 
 clean:
-	rm $(addsuffix kernel.img, $(dirs))
+	rm kernel.img
 
 .PHONY: clean
