1bb2bd87d20d8f6b0009fdc0f52123d73ba27d8e
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2019-10-21 20:54:47 +0300

    Remove the noinitrd case
    Signed-off-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>

--- depthcharge.mk
+++ depthcharge.mk
@@ -1,9 +1,7 @@
 #! /usr/bin/make -f
 
 dirs = $(wildcard */)
-kernel-initrd = $(addsuffix kernel-initrd.img, $(dirs))
-kernel-noinitrd = $(addsuffix kernel-noinitrd.img, $(dirs))
-all: $(kernel-initrd) $(kernel-noinitrd)
+all: $(addsuffix kernel.img, $(dirs))
 
 keys := kernel.keyblock kernel_data_key.vbprivk
 
@@ -19,7 +17,7 @@ keys := kernel.keyblock kernel_data_key.vbprivk
 		--signprivate kernel_data_key.vbprivk
 	test "$$(stat -c '%s' $@)" -lt 33554432
 
-%/kernel-initrd.itb: %/vmlinuz.lz4 %/initrd.img %/rk3399-gru-kevin.dtb
+%/kernel.itb: %/vmlinuz.lz4 %/initrd.img %/rk3399-gru-kevin.dtb
 	mkimage \
 		-D "-I dts -O dtb -p 2048" \
 		-f auto \
@@ -33,42 +31,19 @@ keys := kernel.keyblock kernel_data_key.vbprivk
 		-b $*/rk3399-gru-kevin.dtb \
 		$@
 
-%/kernel-noinitrd.itb: %/vmlinuz.lz4 %/rk3399-gru-kevin.dtb
-	mkimage \
-		-D "-I dts -O dtb -p 2048" \
-		-f auto \
-		-A arm64 \
-		-O linux \
-		-T kernel \
-		-C lz4 \
-		-a 0 \
-		-d $*/vmlinuz.lz4 \
-		-b $*/rk3399-gru-kevin.dtb \
-		$@
-
-%/kernel-initrd.args: initrd.args
-	cat $< | tr "\n" " " > $@
-
-%/kernel-noinitrd.args: noinitrd.args
-	cat $< | tr "\n" " " > $@
-
-%.lz4: %
-	lz4 -12 $< $@
-
-initrd.args:
+%/kernel.args: initrd.args
 	@echo 'root=PARTUUID=4f7a82a0-1e9a-47fd-83b5-73847350f068' > $@
 	@echo 'console=tty1' >> $@
 	@echo 'rootwait' >> $@
 	@echo 'rw' >> $@
 
-noinitrd.args: initrd.args
-	@cp $< $@
-	@echo 'noinitrd' >> $@
+%.lz4: %
+	lz4 -12 $< $@
 
 bootloader.bin:
 	dd if=/dev/zero of=$@ count=1
 
 clean:
-	rm $(kernel-initrd) $(kernel-noinitrd)
+	rm $(addsuffix kernel.img, $(dirs))
 
 .PHONY: clean
