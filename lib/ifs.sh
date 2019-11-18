264f8984a9f62409c32948da43e54aa6cf1d345c
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2019-11-18 17:26:25 +0300

    Move custom IFS to lib/ifs.sh
    Signed-off-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>

---
+++ lib/ifs.sh
@@ -0,0 +1,16 @@
+# Whitespace
+# ----------
+
+ORIG_IFS="$IFS"
+CUSTOM_IFS="$(printf "\n\t")"
+NEWLINE="${CUSTOM_IFS%?}"
+TAB="${CUSTOM_IFS#?}"
+
+has_newline() {
+    case "$1" in
+        *"$NEWLINE"*) return 0 ;;
+        *) return 1 ;;
+    esac
+}
+
+readonly ORIG_IFS CUSTOM_IFS NEWLINE TAB
