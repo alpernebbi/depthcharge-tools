a90a891664cb62fd7ae177b4270a00b9d711c8ea
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2019-11-18 16:31:31 +0300

    Improve message printing
    Uses printf instead of echo, prints messages to stderr, has better
    handling of missing usage. Also adds QUIET and SILENT for suppressing
    warnings and errors.
    
    Signed-off-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>

--- lib/msg.sh
+++ lib/msg.sh
@@ -1,28 +1,41 @@
-# Functions to make printing output easier.
+# Printing output
+# ---------------
+
 msg() {
     if [ "$#" -gt 0 ]; then
-        echo "$0:" "$@"
+        printf "%s " "${PROG:-${0##*/}}:" "$@"
     fi
+    printf "\n"
 }
 
 info() {
-    if [ "$VERBOSE" = yes ]; then
-        msg "$@"
+    if [ "${VERBOSE:-no}" = yes ] && [ "$#" -gt 0 ]; then
+        msg "info:" "$@"
     fi
-}
+} >&2
 
 warn() {
-    msg "warning:" "$@" >&2
-}
+    if [ "${QUIET:-no}" != yes ] && [ "$#" -gt 0 ]; then
+        msg "warning:" "$@"
+    fi
+} >&2
 
 error() {
-    msg "error:" "$@" >&2
-    exit 1
-}
+    if [ "${SILENT:-no}" != yes ] && [ "$#" -gt 0 ]; then
+        msg "error:" "$@"
+    fi
+    return 1
+} >&2
 
 usage_error() {
-    msg "error:" "$@" "\n" >&2
-    usage >&2
+    if [ "${SILENT:-no}" != yes ]; then
+        msg "usage error:" "$@"
+        msg
+        if command -v usage >/dev/null; then
+            usage
+        else
+            printf "%s\n" "${PROG:-${0##*/}} has no usage information."
+        fi
+    fi
     exit 1
-}
-
+} >&2
