ef688224312ed65af2922724ea5a0d511771249e
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2020-11-07 22:14:24 +0300

    mkdepthcharge: Implement argument parser
    This tries to match the earlier version's arguments, and does nothing
    except print the arguments for now.
    
    Signed-off-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>

--- depthcharge_tools/mkdepthcharge.py
+++ depthcharge_tools/mkdepthcharge.py
@@ -11,19 +11,132 @@ logger = logging.getLogger(__name__)
 
 def main(*argv):
     args = parse_args(*argv)
+    print(args)
 
 
 def parse_args(*argv):
     parser = argparse.ArgumentParser(
-        description="Build boot images for the ChromeOS firmware.",
-        add_help=True,
+        description="Build boot images for the ChromeOS bootloader.",
+        usage="%(prog)s [options] -o FILE [--] vmlinuz [initramfs] [dtb ...]",
+        add_help=False,
     )
 
-    parser.add_argument(
+    input_files = parser.add_argument_group(
+        title="Input files",
+    )
+    input_files.add_argument(
+        "vmlinuz",
+        help="Kernel executable",
+    )
+    input_files.add_argument(
+        "initramfs",
+        nargs="?",
+        help="Ramdisk image",
+    )
+    input_files.add_argument(
+        "dtb",
+        nargs="*",
+        default=[],
+        help="Device-tree binary file",
+    )
+
+    options = parser.add_argument_group(
+        title="Options",
+    )
+    options.add_argument(
+        "-h", "--help",
+        action='help',
+        help="Show this help message.",
+    )
+    options.add_argument(
         "--version",
         action='version',
-        version="%(prog)s {}".format(__version__),
-        help="print version information and exit",
+        version="depthcharge-tools %(prog)s {}".format(__version__),
+        help="Print program version.",
+    )
+    options.add_argument(
+        "-v", "--verbose",
+        action='store_true',
+        help="Print more detailed output.",
+    )
+    options.add_argument(
+        "-o", "--output",
+        metavar="FILE",
+        action='store',
+        required=True,
+        help="Write resulting image to FILE.",
+    )
+    options.add_argument(
+        "-A", "--arch",
+        metavar="ARCH",
+        action='store',
+        choices=["arm", "arm64", "aarch64", "x86", "x86_64", "amd64"],
+        help="Architecture to build for.",
+    )
+    options.add_argument(
+        "--format",
+        dest="image_format",
+        metavar="FORMAT",
+        action='store',
+        choices=["fit", "zimage"],
+        help="Kernel image format to use.",
+    )
+
+    fit_options = parser.add_argument_group(
+        title="FIT image options",
+    )
+    fit_options.add_argument(
+        "-C", "--compress",
+        metavar="TYPE",
+        action='store',
+        choices=["none", "lz4", "lzma"],
+        help="Compress vmlinuz file before packing.",
+    )
+    fit_options.add_argument(
+        "-n", "--name",
+        metavar="DESC",
+        action='store',
+        help="Description of vmlinuz to put in the FIT.",
+    )
+
+    vboot_options = parser.add_argument_group(
+        title="Depthcharge image options",
+    )
+    vboot_options.add_argument(
+        "-c", "--cmdline",
+        metavar="CMD",
+        action='append',
+        help="Command-line parameters for the kernel.",
+    )
+    vboot_options.add_argument(
+        "--no-kern-guid",
+        dest='kern_guid',
+        action='store_false',
+        help="Don't prepend kern_guid=%%U to the cmdline.",
+    )
+    vboot_options.add_argument(
+        "--bootloader",
+        metavar="FILE",
+        action='store',
+        help="Bootloader stub binary to use.",
+    )
+    vboot_options.add_argument(
+        "--devkeys",
+        metavar="DIR",
+        action='store',
+        help="Directory containing developer keys to use.",
+    )
+    vboot_options.add_argument(
+        "--keyblock",
+        metavar="FILE",
+        action='store',
+        help="The key block file (.keyblock).",
+    )
+    vboot_options.add_argument(
+        "--signprivate",
+        metavar="FILE",
+        action='store',
+        help="Private key (.vbprivk) to sign the image.",
     )
 
     return parser.parse_args(*argv[1:])
