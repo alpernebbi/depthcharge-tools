8e2b642cd56fde7a195b8b2b350de8d56e899535
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2020-11-07 18:45:07 +0300

    setup.py: Read version from __version__ in init.py
    I want this to be the single source for the version number.
    
    Signed-off-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>

--- depthcharge_tools/__init__.py
+++ depthcharge_tools/__init__.py
@@ -1,1 +1,3 @@
 #! /usr/bin/env python3
+
+__version__ = 'v0.5.0-dev'
--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 #! /usr/bin/env python3
 
 import os
+import re
 import setuptools
 
 root = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(root, 'README.rst'), encoding='utf-8') as f:
     readme = f.read()
 
+def version(module):
+    init_py = os.path.join(root, module, '__init__.py')
+    pattern = re.compile('^__version__\s*=\s*(".*"|\'.*\')$')
+    with open(init_py, encoding='utf-8') as f:
+        for match in filter(bool, map(pattern.match, f)):
+            return match.group(1)[1:-1]
+
 setuptools.setup(
     name='depthcharge-tools',
-    version='v0.5.0-dev',
+    version=version("depthcharge_tools"),
     description='Tools to manage the Chrome OS bootloader',
     long_description=readme,
     long_description_content_type="text/x-rst",
