3336491b868a6d286f36a8634ccbbcf89abae28a
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2020-11-16 03:02:27 +0300

    depthchargectl: Initial argument parser
    Tries to match the earlier version's arguments and help message, but
    running the commands just print out the arguments the command received.
    
    Signed-off-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>

--- depthcharge_tools/depthchargectl.py
+++ depthcharge_tools/depthchargectl.py
@@ -95,10 +95,35 @@ def argument_parser():
         "build",
         description="Buld a depthcharge image for the running system.",
         help="Buld a depthcharge image for the running system.",
+        usage="%(prog)s [options] [kernel-version]",
         add_help=False,
+    )
+    build_arguments = build.add_argument_group(
+        title="Positional arguments",
+    )
+    build_arguments.add_argument(
+        "kernel_version",
+        metavar="kernel-version",
+        nargs="?",
+        help="Installed kernel version to build an image for.",
     )
     build_options = build.add_argument_group(
         title="Options",
+    )
+    build_options.add_argument(
+        "-a", "--all",
+        action='store_true',
+        help="Build images for all available kernel versions.",
+    )
+    build_options.add_argument(
+        "-f", "--force",
+        action='store_true',
+        help="Rebuild images even if existing ones are valid.",
+    )
+    build_options.add_argument(
+        "--reproducible",
+        action='store_true',
+        help="Try to build reproducible images.",
     )
     add_global_options(build_options)
 
@@ -106,7 +131,16 @@ def argument_parser():
         "check",
         description="Check if a depthcharge image can be booted.",
         help="Check if a depthcharge image can be booted.",
+        usage="%(prog)s [options] image",
         add_help=False,
+    )
+    check_arguments = check.add_argument_group(
+        title="Positional arguments",
+    )
+    check_arguments.add_argument(
+        "image",
+        nargs="?",
+        help="Depthcharge image to check validity of.",
     )
     check_options = check.add_argument_group(
         title="Options",
@@ -117,10 +151,27 @@ def argument_parser():
         "partitions",
         description="List ChromeOS kernel partitions.",
         help="List ChromeOS kernel partitions.",
+        usage="%(prog)s [options] [disk ...]",
         add_help=False,
     )
     partitions_options = partitions.add_argument_group(
         title="Options",
+    )
+    partitions_options.add_argument(
+        "-n", "--noheadings",
+        action='store_true',
+        help="Don't print column headings.",
+    )
+    partitions_options.add_argument(
+        "-a", "--all-disks",
+        action='store_true',
+        help="List partitions on all disks.",
+    )
+    partitions_options.add_argument(
+        "-o", "--output",
+        metavar="COLUMNS",
+        action='append',
+        help="Comma separated list of columns to output.",
     )
     add_global_options(partitions_options)
 
@@ -128,10 +179,32 @@ def argument_parser():
         "rm",
         description="Remove images and disable partitions containing them.",
         help="Remove images and disable partitions containing them.",
+        usage="%(prog)s [options] (kernel-version | image)",
         add_help=False,
+    )
+    rm_arguments = rm.add_argument_group(
+        title="Positional arguments",
+    )
+    rm_image_or_version = rm_arguments.add_mutually_exclusive_group(
+            required=True,
+    )
+    rm_image_or_version.add_argument(
+        "kernel-version",
+        nargs="?",
+        help="Installed kernel version to disable.",
+    )
+    rm_image_or_version.add_argument(
+        "image",
+        nargs="?",
+        help="Depthcharge image to disable.",
     )
     rm_options = rm.add_argument_group(
         title="Options",
+    )
+    rm_options.add_argument(
+        "-f", "--force",
+        action='store_true',
+        help="Allow removing the currently booted partition.",
     )
     add_global_options(rm_options)
 
@@ -139,6 +212,7 @@ def argument_parser():
         "set-good",
         description="Set the current partition as successfully booted.",
         help="Set the current partition as successfully booted.",
+        usage="%(prog)s [options]",
         add_help=False,
     )
     set_good_options = set_good.add_argument_group(
@@ -150,10 +224,38 @@ def argument_parser():
         "target",
         description="Choose or validate a ChromeOS Kernel partition to use.",
         help="Choose or validate a ChromeOS Kernel partition to use.",
+        usage="%(prog)s [options] [partition | disk ...]",
         add_help=False,
+    )
+    target_arguments = target.add_argument_group(
+        title="Positional arguments",
+    )
+    target_image_or_version = target_arguments.add_mutually_exclusive_group(
+            required=False,
+    )
+    target_image_or_version.add_argument(
+        "partition",
+        nargs="?",
+        help="Chrome OS kernel partition to validate.",
+    )
+    target_image_or_version.add_argument(
+        "disk",
+        nargs="?",
+        help="Disks to search for an appropriate Chrome OS kernel partition.",
     )
     target_options = target.add_argument_group(
         title="Options",
+    )
+    target_options.add_argument(
+        "-s", "--min-size",
+        metavar="BYTES",
+        action='store',
+        help="Target partitions larger than this size.",
+    )
+    target_options.add_argument(
+        "--allow-current",
+        action='store_true',
+        help="Allow targeting the currently booted partition.",
     )
     add_global_options(target_options)
 
@@ -161,10 +263,49 @@ def argument_parser():
         "write",
         description="Write an image to a ChromeOS kernel partition.",
         help="Write an image to a ChromeOS kernel partition.",
+        usage="%(prog)s [options] (kernel-image | image)",
         add_help=False,
+    )
+    write_arguments = write.add_argument_group(
+        title="Positional arguments",
+    )
+    write_image_or_version = write_arguments.add_mutually_exclusive_group(
+            required=True,
+    )
+    write_image_or_version.add_argument(
+        "kernel-version",
+        nargs="?",
+        help="Installed kernel version to write to disk.",
+    )
+    write_image_or_version.add_argument(
+        "image",
+        nargs="?",
+        help="Depthcharge image to write to disk.",
     )
     write_options = write.add_argument_group(
         title="Options",
+    )
+    write_options.add_argument(
+        "-f", "--force",
+        action='store_true',
+        help="Write image even if it cannot be verified.",
+    )
+    write_options.add_argument(
+        "-t", "--target",
+        metavar="DISK|PART",
+        action='store',
+        help="Specify a disk or partition to write to.",
+    )
+    write_options.add_argument(
+        "--no-prioritize",
+        dest="prioritize",
+        action='store_false',
+        help="Don't set any flags on the partition.",
+    )
+    write_options.add_argument(
+        "--allow-current",
+        action='store_true',
+        help="Allow overwriting the currently booted partition.",
     )
     add_global_options(write_options)
 
