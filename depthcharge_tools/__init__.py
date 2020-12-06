601607b245d47d2a991b19db0b525ab1d522fc7d
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2020-12-06 18:45:09 +0300

    depthcharge-tools: Parse the config and board info at module init
    I'm running this from the repository root while developing, so use the
    files in the "conf" directory. I'll eventually need to update these to
    the distro paths and hopefully detect editable installs for development.
    
    Signed-off-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>

--- depthcharge_tools/__init__.py
+++ depthcharge_tools/__init__.py
@@ -1,8 +1,21 @@
 #! /usr/bin/env python3
 
 import logging
+import pathlib
+
+from depthcharge_tools.utils import (
+    Config,
+    BoardInfo,
+)
+
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
 __version__ = 'v0.5.0-dev'
+
+DATADIR = pathlib.Path("conf")
+SYSCONFDIR = pathlib.Path("conf")
+
+config = Config(SYSCONFDIR / "config")
+boards = BoardInfo(DATADIR / "db", DATADIR / "userdb")
