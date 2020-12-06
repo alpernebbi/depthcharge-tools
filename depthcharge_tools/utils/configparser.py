b8073beb536fb62a2862a1f9b2f7b75a3512ea71
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2020-12-06 18:38:02 +0300

    utils: configparser: Convert config and board info parsing to classes
    Primarily so that I can define and use properties for each config and
    board info option. I considered making them subclasses of ConfigParser,
    but I would need to do too much wrapping to make BoardInfo a container
    of Boards and to make Config act as a section while being a config.
    
    Signed-off-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>

--- depthcharge_tools/utils/configparser.py
+++ depthcharge_tools/utils/configparser.py
@@ -5,19 +5,80 @@ import pathlib
 import re
 
 
-def read_machinedb():
-    parser = configparser.ConfigParser()
-    parser.SECTCRE = re.compile("^Machine: (?P<header>.*)$")
+class BoardInfo:
+    def __init__(self, *paths):
+        parser = configparser.ConfigParser()
+        parser.SECTCRE = re.compile("^Machine: (?P<header>.*)$")
+        parser.read(paths)
 
-    datadir = pathlib.Path("conf")
-    parser.read([datadir / "db", datadir / "userdb"])
-    return parser
+        self._parser = parser
+
+    def __getitem__(self, board):
+        section = self._parser[board]
+        return Board(section)
 
 
-def read_config():
-    parser = configparser.ConfigParser()
+class Board:
+    def __init__(self, info):
+        self._info = info
 
-    confdir = pathlib.Path("conf")
-    config = confdir / "config"
-    parser.read_string("\n".join(("[DEFAULT]", config.read_text())))
-    return parser
+    @property
+    def name(self):
+        return self._info.name
+
+    @property
+    def dtb_name(self):
+        return self._info.get("dtb-name")
+
+    @property
+    def kernel_compression(self):
+        return self._info.get("kernel-compression")
+
+    @property
+    def max_size(self):
+        return self._info.get("max-size")
+
+    @property
+    def image_format(self):
+        return self._info.get("image-format")
+
+
+class Config:
+    def __init__(self, *paths):
+        parser = configparser.ConfigParser()
+        for path in paths:
+            parser.read_string(
+                "\n".join(("[CONFIG]", pathlib.Path(path).read_text())),
+                source=path,
+            )
+
+        self._parser = parser
+        self._config = parser["CONFIG"]
+
+    @property
+    def machine(self):
+        return self._config.get("machine")
+
+    @property
+    def kernel_cmdline(self):
+        return self._config.get("kernel-cmdline")
+
+    @property
+    def kernel_compression(self):
+        return self._config.get("kernel-compression")
+
+    @property
+    def ignore_initramfs(self):
+        return self._config.get("ignore-initramfs")
+
+    @property
+    def vboot_keyblock(self):
+        return self._config.get("vboot-keyblock")
+
+    @property
+    def vboot_public_key(self):
+        return self._config.get("vboot-public-key")
+
+    @property
+    def vboot_private_key(self):
+        return self._config.get("vboot-private-key")
