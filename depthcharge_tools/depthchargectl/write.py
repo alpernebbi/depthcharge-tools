2133877272fe81e8b82c3fe66cbb84afe6ebe8d9
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2020-12-13 20:08:02 +0300

    depthchargectl: write: Add initial implementation
    Writing to disk works, but as it uses python's write_bytes we need to
    call python as root. It can't write into disk images yet, as they don't
    correspond to actual files.
    
    Also combine both "kernel-version" function argument into "image", while
    keeping it in the help message.
    
    Signed-off-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>

--- depthcharge_tools/depthchargectl/write.py
+++ depthcharge_tools/depthchargectl/write.py
@@ -3,9 +3,14 @@
 import argparse
 import logging
 
-from depthcharge_tools import __version__
+from depthcharge_tools import (
+    __version__,
+    LOCALSTATEDIR,
+)
 from depthcharge_tools.utils import (
     Command,
+    Kernel,
+    Path,
 )
 
 logger = logging.getLogger(__name__)
@@ -15,8 +20,51 @@ class DepthchargectlWrite(Command):
     def __init__(self, name="depthchargectl write", parent=None):
         super().__init__(name, parent)
 
-    def __call__(self, *args, **kwargs):
-        print(args, kwargs)
+    def __call__(
+        self,
+        image=None,
+        force=False,
+        target=None,
+        prioritize=True,
+        allow_current=False,
+    ):
+        kernels = Kernel.all()
+
+        if image is None:
+            image = self._parent.build(max(kernels).release)
+
+        elif isinstance(image, str):
+            for k in kernels:
+                if image == k.release:
+                    image = self._parent.build(k.release)
+                    break
+            else:
+                image = Path(image).resolve()
+
+        try:
+            self._parent.check(image)
+        except Exception as err:
+            if not force:
+                raise
+
+        target = self._parent.target(
+            disks=[target] if target else None,
+            min_size=image.stat().st_size,
+            allow_current=allow_current,
+        )
+
+        if target is None:
+            raise RuntimeError("target")
+
+        if target.path is None:
+            raise RuntimeError("target path")
+
+        target.attribute = 0x010
+        target.path.write_bytes(image.read_bytes())
+
+        if prioritize:
+            target.prioritize()
+
 
     def _init_parser(self):
         return super()._init_parser(
@@ -26,15 +74,13 @@ class DepthchargectlWrite(Command):
         )
 
     def _init_arguments(self, arguments):
-        image_or_version = arguments.add_mutually_exclusive_group(
-                required=True,
-        )
-        image_or_version.add_argument(
-            "kernel-version",
-            nargs="?",
+        arguments.add_argument(
+            dest=argparse.SUPPRESS,
+            metavar="kernel-version",
+            nargs=argparse.SUPPRESS,
             help="Installed kernel version to write to disk.",
         )
-        image_or_version.add_argument(
+        arguments.add_argument(
             "image",
             nargs="?",
             help="Depthcharge image to write to disk.",
