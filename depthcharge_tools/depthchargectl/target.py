24635f3526fdbf879be24c2d60a8436890e0cbaa
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2020-12-19 20:57:34 +0300

    depthchargectl: target: Add comments and logging messages
    This copies what we can from the shell version, but things are quite
    different than the shell version in their semantics, mainly in checking
    validity of a partition and exiting with a status code for whatever
    condition it fails. Ideally, that would be done in the Partition helper
    class and an error would be raised there for partition validity. Though
    things like checking partition size and currently booted partition is
    appropriate here.
    
    Signed-off-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>

--- depthcharge_tools/depthchargectl/target.py
+++ depthcharge_tools/depthchargectl/target.py
@@ -25,32 +25,55 @@ class DepthchargectlTarget(Command):
         min_size=None,
         allow_current=False,
     ):
+        # Disks containing /boot and / should be available during boot,
+        # so we target only them by default.
         if not disks:
             disks = Disk.disks(bootable=True)
 
         if not disks:
-            raise ValueError("no-disks")
+            raise ValueError(
+                "Couldn't find a real disk containing root or boot."
+            )
 
+        # The inputs can be a mixed list of partitions and disks,
+        # separate the two.
         partitions = []
         for d in list(disks):
             try:
                 partitions.append(Partition(d))
+                logger.info("Using target '{}' as a partition.".format(d))
                 disks.remove(d)
             except:
                 pass
 
+        # For arguments which are disks, search all their partitions.
         if disks:
+            logger.info("Finding disks for targets '{}'.".format(disks))
             for d in Disk.disks(*disks):
+                logger.info("Using '{}' as a disk.".format(d))
                 partitions.extend(d.partitions())
 
         good_partitions = []
         for p in partitions:
             if min_size is not None and p.size < int(min_size):
+                logger.info(
+                    "Skipping partition '{}' as too small."
+                    .format(p)
+                )
                 continue
+
             if not allow_current and p.path == Disk.by_kern_guid():
+                logger.info(
+                    "Skipping currently booted partition '{}'."
+                    .format(p)
+                )
                 continue
+
+            logger.info("Partition '{}' is usable.".format(p))
             good_partitions.append(p)
 
+        # Get the least-successful, least-priority, least-tries-left
+        # partition in that order of preference.
         good_partitions = sorted(
             good_partitions,
             key=lambda p: (p.successful, p.priority, p.tries, p.size),
