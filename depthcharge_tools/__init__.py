486c2eec09c865ebe9566531fa2f7687f9f28ec9
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2020-12-30 01:14:47 +0300

    depthcharge-tools: Distinguish between repo and installed package
    The paths for the config and db files etc. will need to be set
    differently when running things during development vs. when running from
    an installed version.
    
    For now, implement getting the package name and version from the
    installed package metadata or the git describe string. Other variables
    will probably need to be inserted at build-time as a resource, then
    retrieved using pkg_resources somehow.
    
    This doesn't distinguish between the actual installation and a
    develop-mode installation (with "pip install -e .") yet.
    
    Signed-off-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>

--- depthcharge_tools/__init__.py
+++ depthcharge_tools/__init__.py
@@ -2,17 +2,52 @@
 
 import logging
 import pathlib
+import pkg_resources
+import subprocess
+import sys
 
 from depthcharge_tools.utils import (
     Config,
     BoardInfo,
 )
 
+try:
+    self = pkg_resources.require(__name__)[0]
+    PACKAGENAME = self.project_name
+    VERSION = self.version
+    GITHASH = None
+
+except pkg_resources.DistributionNotFound:
+    PACKAGENAME = "depthcharge-tools"
+    VERSION = None
+    GITHASH = None
+
+    for path in sys.path:
+        path = pathlib.Path(path).resolve()
+
+        init = path / "depthcharge_tools" / "__init__.py"
+        if not init.exists():
+            continue
+
+        git = path / ".git"
+        if git.exists():
+            proc = subprocess.run(
+                ["git", "-C", path, "describe"],
+                stdout=subprocess.PIPE,
+                encoding="utf-8",
+                check=False,
+            )
+            if proc.returncode == 0:
+                VERSION, _ , GITHASH = proc.stdout.partition("-g")
+
+        break
+
 
 logger = logging.getLogger(__name__)
 log_handler = logging.StreamHandler()
 logger.addHandler(log_handler)
 
+VERSION = pkg_resources.parse_version(VERSION)
 __version__ = 'v0.5.0-dev'
 
 DATADIR = pathlib.Path("conf").resolve()
