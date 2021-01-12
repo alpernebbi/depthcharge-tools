248215a8fc1ccb336a5d7c78395e99f6da990d50
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2021-01-12 22:46:50 +0300

    depthcharge-tools: Reorder __init__.py
    Keep the logger part on top same as the other files, put version
    derivation into a function, remove unused imports.
    
    Signed-off-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>

--- depthcharge_tools/__init__.py
+++ depthcharge_tools/__init__.py
@@ -1,56 +1,59 @@
 #! /usr/bin/env python3
 
-import configparser
-import logging
 import glob
+import logging
 import pathlib
 import pkg_resources
 import re
 import subprocess
-import sys
 
 from depthcharge_tools.utils import (
     Config,
     BoardInfo,
 )
 
-pkg_path = pkg_resources.resource_filename(__name__, '')
-pkg_path = pathlib.Path(pkg_path).resolve()
-
-try:
-    self = pkg_resources.require(__name__)[0]
-    PACKAGENAME = self.project_name
-    VERSION = self.version
-
-except pkg_resources.DistributionNotFound:
-    PACKAGENAME = "depthcharge-tools"
-    VERSION = None
-
-    setup_py = pkg_path.parent / "setup.py"
-    if setup_py.exists():
-        VERSION = re.findall(
-            'version=(\'.+\'|".+"),',
-            setup_py.read_text(),
-        )[0].strip('"\'')
-
-if (pkg_path.parent / ".git").exists():
-    proc = subprocess.run(
-        ["git", "-C", pkg_path, "describe"],
-        stdout=subprocess.PIPE,
-        encoding="utf-8",
-        check=False,
-    )
-    if proc.returncode == 0:
-        tag, *local = proc.stdout.split("-")
-        VERSION = "{}+{}".format(tag, ".".join(local)) if local else tag
-
 logger = logging.getLogger(__name__)
 log_handler = logging.StreamHandler()
 logger.addHandler(log_handler)
 
-if VERSION is not None:
-    VERSION = pkg_resources.parse_version(VERSION)
-__version__ = VERSION
+
+def get_version():
+    version = None
+    pkg_path = pkg_resources.resource_filename(__name__, '')
+    pkg_path = pathlib.Path(pkg_path).resolve()
+
+    try:
+        self = pkg_resources.require(__name__)[0]
+        version = self.version
+
+    except pkg_resources.DistributionNotFound:
+        setup_py = pkg_path.parent / "setup.py"
+        if setup_py.exists():
+            version = re.findall(
+                'version=(\'.+\'|".+"),',
+                setup_py.read_text(),
+            )[0].strip('"\'')
+
+    if (pkg_path.parent / ".git").exists():
+        proc = subprocess.run(
+            ["git", "-C", pkg_path, "describe"],
+            stdout=subprocess.PIPE,
+            encoding="utf-8",
+            check=False,
+        )
+        if proc.returncode == 0:
+            tag, *local = proc.stdout.split("-")
+
+            if local:
+                version = "{}+{}".format(tag, ".".join(local))
+            else:
+                version = tag
+
+    if version is not None:
+        return pkg_resources.parse_version(version)
+
+__version__ = get_version()
+
 
 config_files = [
     pkg_resources.resource_filename(__name__, "config"),
