076b8ce0a60feb61760a609d63db98e00ae71850
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2021-01-14 20:54:17 +0300

    config: Implement inheritance for config sections
    This implements a custom dictionary that finds all parents for a key,
    combines their values in topological order to a ChainMap and returns
    that when the values are dictionaries.
    
    We can't do this in our wrapper Config class since ConfigParser prefers
    default values to fallback values, making it impossible to determine
    whether a value retrieved from a child section is set in the child
    section or the default section.
    
    Now that inheritance works, remove values from the config file which can
    be inherited from parent boards.
    
    Signed-off-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>

--- depthcharge_tools/__init__.py
+++ depthcharge_tools/__init__.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+import collections
 import configparser
 import glob
 import logging
@@ -51,9 +52,45 @@ def get_version():
 __version__ = get_version()
 
 
+# Inheritance for config sections
+class ConfigDict(collections.OrderedDict):
+    def __getitem__(self, key):
+        super_ = super()
+        if not isinstance(key, str) or "/" not in key:
+            return super_.__getitem__(key)
+
+        def getitem(key):
+            try:
+                return super_.__getitem__(key)
+            except KeyError:
+                return KeyError
+
+        def parents(leaf):
+            idx = leaf.find("/")
+            while idx != -1:
+                yield leaf[:idx]
+                idx = leaf.find("/", idx + 1)
+            yield leaf
+
+        items = list(
+            item for item in reversed([
+                getitem(p) for p in parents(key)
+            ]) if item != KeyError
+        )
+
+        if all(isinstance(i, dict) for i in items):
+            return collections.ChainMap(*items)
+
+        if items:
+            return items[0]
+
+        raise KeyError(key)
+
+
 def read_config(*paths):
     parser = configparser.ConfigParser(
         default_section="depthcharge-tools",
+        dict_type=ConfigDict,
     )
 
     config_ini = pkg_resources.resource_string(__name__, "config.ini")
