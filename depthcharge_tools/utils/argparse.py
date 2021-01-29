66d80b3ac6e5ed02f6fa9fcd4a23cb6a6e3837eb
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2021-01-29 16:10:49 +0300

    utils: argparse: Pass only acceptable kwargs to Action __init__
    I'll eventually want to pass custom and even arbitrary keyword arguments
    to FunctionBindAction, so we have to filter them out before passing
    those to the parent class.
    
    Signed-off-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>

--- depthcharge_tools/utils/argparse.py
+++ depthcharge_tools/utils/argparse.py
@@ -63,7 +63,8 @@ class FunctionBindAction(argparse.Action):
     def __init__(self, option_strings, dest, func, **kwargs):
         self.signature = inspect.signature(func)
 
-        super().__init__(option_strings, dest, **kwargs)
+        super_kwargs = filter_action_kwargs(kwargs)
+        super().__init__(option_strings, dest, **super_kwargs)
 
     def __call__(self, parser, namespace, values, option_string=None):
         bound = self.signature.bind(*values)
