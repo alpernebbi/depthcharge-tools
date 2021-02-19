772ec65d14eeb60476becc3aca923bf146c49359
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2021-02-19 20:02:10 +0300

    depthchargectl: bless: Add partition as positional argument
    Still defaults to the currently booted partition.
    
    Signed-off-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>

--- depthcharge_tools/depthchargectl/_bless.py
+++ depthcharge_tools/depthchargectl/_bless.py
@@ -22,30 +22,41 @@ logger = logging.getLogger(__name__)
 class depthchargectl_bless(
     depthchargectl,
     prog="depthchargectl bless",
-    usage="%(prog)s [options]",
+    usage="%(prog)s [options] [PARTITION]",
     add_help=False,
 ):
-    """Set the current partition as successfully booted."""
+    """Set the active or given partition as successfully booted."""
 
     config_section = "depthchargectl/bless"
 
-    def __call__(self):
-        try:
-            part = Partition(Disk.by_kern_guid())
-        except:
-            raise ValueError(
-                "Couldn't figure out the currently booted partition."
-            )
+    @Group
+    def positionals(self):
+        """Positional arguments"""
 
+    @positionals.add
+    @Argument(metavar="PARTITION")
+    def partition(self, device=None):
+        """ChromeOS Kernel partition to manage"""
+        if device is None:
+            try:
+                device = Disk.by_kern_guid()
+            except:
+                raise ValueError(
+                    "Couldn't figure out the currently booted partition."
+                )
+
+        return Partition(device)
+
+    def __call__(self):
         logger.info(
             "Setting '{}' as the highest-priority bootable part."
-            .format(part)
+            .format(self.partition)
         )
-        part.attribute = 0x111
-        part.prioritize()
+        self.partition.attribute = 0x111
+        self.partition.prioritize()
         logger.info(
             "Set '{}' as next to boot and successful."
-            .format(part)
+            .format(self.partition)
         )
 
     global_options = depthchargectl.global_options
