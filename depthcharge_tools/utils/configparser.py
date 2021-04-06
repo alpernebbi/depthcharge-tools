e69d02113d244bf8bfba786b5dbec3049bc0d8c1
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2021-04-06 19:50:41 +0300

    utils: configparser: Add helper to analyze chromiumos project repo
    This is meant to be run over the chromiumos/project repo [1]. It looks
    like part of a relatively new framework the build system is moving to.
    Similar to the board-overlays analyzer this only returns some board
    relations right now.
    
    [1] https://chromium.googlesource.com/chromiumos/project/
    
    Signed-off-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>

--- depthcharge_tools/utils/configparser.py
+++ depthcharge_tools/utils/configparser.py
@@ -2,6 +2,7 @@
 
 import configparser
 import collections
+import json
 import logging
 import re
 
@@ -164,5 +165,44 @@ def analyze_board_overlays(path):
         layout_conf = parse_layout_conf(board_d)
         for parent in layout_conf.get("masters", "").split():
             add_parent(parent, repo_name)
+
+    return board_relations
+
+
+def analyze_chromiumos_project(path):
+    board_relations = DirectedGraph()
+    project = Path(path)
+
+    def get_project_config_boards(d):
+        children = set()
+        project_config = (
+            d / "sw_build_config" / "platform" / "chromeos-config"
+            / "generated" / "project-config.json"
+        )
+        if project_config.is_file():
+            config = json.loads(project_config.read_text())
+
+            for section in config["chromeos"]["configs"]:
+                if section["name"]:
+                    children.add(section["name"])
+
+        return children
+
+    for board in iterdir(project):
+        if not board.is_dir() or board.name.startswith("."):
+            continue
+
+        for profile in iterdir(board):
+            if not profile.is_dir() or profile.name.startswith("."):
+                continue
+
+            # puff/puff exists
+            if profile.name != board.name:
+                board_relations.add_edge(board.name, profile.name)
+
+            for child in get_project_config_boards(profile):
+                # galaxy/{andromeda,sombrero} has galaxy
+                if child != profile.name and child != board.name:
+                    board_relations.add_edge(profile.name, child)
 
     return board_relations
