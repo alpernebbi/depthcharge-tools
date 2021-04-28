eebf010c8dcfac4335ca5b75dc014a4eac7c2090
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2021-04-28 11:30:40 +0300

    completions: zsh: Update for v0.5.0
    Signed-off-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>

--- completions/_depthchargectl.zsh
+++ completions/_depthchargectl.zsh
@@ -4,17 +4,40 @@ function _depthchargectl {
     _arguments -C \
         {-h,--help}'[Show a help message.]' \
         {-v,--verbose}'[Print more detailed output.]' \
-        --version'[Print program version.]' \
-        '1:command:(build check partitions rm set-good target write)' \
+        {-V,--version}'[Print program version.]' \
+        --tmpdir'[Directory to keep temporary files.]:temp dir:_directories' \
+        --config'[Additional configuration file to read]:config file:_files' \
+        --board'[Assume running on the specified board]:board codenames:{_depthchargectl__board;}' \
+        --images-dir'[Directory to store built images]:images dir:_directories' \
+        --vboot-keyblock'[Keyblock file to include in images]:keyblock file:_files' \
+        --vboot-public-key'[Public key file to verify images]:vbpubk file:_files' \
+        --vboot-private-key'[Private key file to include in images]:vbprivk file:_files' \
+        --kernel-cmdline'[Command line options for the kernel]:kernel cmdline:{_depthchargectl__cmdline;}' \
+        --ignore-initramfs'[Do not include initramfs in images]' \
+        '1:command:(bless build check list remove target write)' \
         '*::arg:->args' \
         ;
 
     case "$state:$line[1]" in
+        args:bless)
+            _arguments -S \
+                --bad'[Set the partition as unbootable]' \
+                --oneshot'[Set the partition to be tried once]' \
+                ':partition:{_depthchargectl__disk}' \
+                ;
+            ;;
         args:build)
             _arguments -S \
-                {-a,--all}'[Rebuild images for all kernel versions.]' \
-                {-f,--force}'[Rebuild images even if unnecessary,]' \
-                --reproducible'[Try to build a reproducible image.]' \
+                --description'[Human-readable description for the image]:image description:($(source /etc/os-release; echo "$NAME"))' \
+                --root'[Root device to add to kernel cmdline]:root device:{_depthchargectl__root; _depthchargectl__disk}' \
+                --compress'[Compression types to attempt]:compress:(none lz4 lzma)' \
+                --timestamp'[Build timestamp for the image]:timestamp:($(date "+%s"))' \
+                --output'[Output image to path instead of storing in images-dir]:output path:_files' \
+                --kernel-release'[Release name for the kernel used in image name]:kernel release:{_depthchargectl__kernel;}' \
+                --kernel'[Kernel executable]:kernel:_files' \
+                --initramfs'[Ramdisk image]:initramfs:_files' \
+                --fdtdir'[Directory to search device-tree binaries for the board]:fdtdir:_directories' \
+                --dtbs'[Device-tree binary files to use instead of searching fdtdir]:dtb files:_files' \
                 ':kernel version:{_depthchargectl__kernel}' \
                 ;
             ;;
@@ -23,8 +46,8 @@ function _depthchargectl {
                 ':image file:_files' \
                 ;
             ;;
-        args:partitions)
-            local outputspec='{_values -s , "description" "S" "SUCCESSFUL" "T" "TRIES" "P" "PRIORITY" "DEVICE" "SIZE"}'
+        args:list)
+            local outputspec='{_values -s , "description" "A" "ATTRIBUTE" "S" "SUCCESSFUL" "T" "TRIES" "P" "PRIORITY" "PATH" "DISKPATH" "DISK" "PARTNO" "SIZE"}'
             _arguments -S \
                 {-n,--noheadings}'[Do not print column headings.]' \
                 {-a,--all-disks}'[list partitions on all disks.]' \
@@ -32,14 +55,11 @@ function _depthchargectl {
                 '*::disk or partition:{_depthchargectl__disk}' \
                 ;
             ;;
-        args:rm)
+        args:remove)
             _arguments -S \
-                {-f,--force}'[Allow removing the current partition.]' \
+                {-f,--force}'[Allow disabling the current partition.]' \
                 '::kernel version or image file:{_depthchargectl__kernel; _files}' \
                 ;
-            ;;
-        args:set-good)
-            : # No options or arguments
             ;;
         args:target)
             _arguments -S \
@@ -72,6 +92,27 @@ function _depthchargectl__kernel {
 function _depthchargectl__disk {
     local disks=($(lsblk -o "PATH" -n -l))
     _describe 'disk or partition' disks
+}
+
+function _depthchargectl__board {
+    local script=(
+        'import re;'
+        'from depthcharge_tools import boards_ini;'
+        'boards = re.findall("codename = (.+)", boards_ini);'
+        'print(*sorted(boards));'
+    )
+    local boards=($(python3 -c "$script"))
+    _describe 'board codenames' boards
+}
+
+function _depthchargectl__cmdline {
+    local cmdline=($(cat /proc/cmdline | sed -e 's/\(cros_secure\|kern_guid\)[^ ]* //g'))
+    _describe 'kernel cmdline' cmdline
+}
+
+function _depthchargectl__root {
+    local root=($(findmnt --fstab -n -o SOURCE "/"))
+    _describe root root
 }
 
 _depthchargectl "$@"
--- completions/_mkdepthcharge.zsh
+++ completions/_mkdepthcharge.zsh
@@ -2,20 +2,25 @@
 
 function _mkdepthcharge {
     _arguments -S \
+        {-d,--vmlinuz}'[Kernel executable]:vmlinuz file:_files' \
+        {-i,--initramfs}'[Ramdisk image]:initrd file:_files' \
+        {-b,--dtbs}'[Device-tree binary files]:dtbs files:_files' \
         {-h,--help}'[Show a help message.]' \
         {-v,--verbose}'[Print more detailed output.]' \
-        --version'[Print program version.]' \
+        {-V,--version}'[Print program version.]' \
+        --tmpdir'[Directory to keep temporary files.]:temp dir:_directories' \
         {-o,--output}'[Write resulting image to FILE.]:output:_files' \
         {-A,--arch}'[Architecture to build for.]:arch:(arm arm64 aarch64 x86 x86_64 amd64)' \
         --format'[Kernel image format to use.]:format:(fit zimage)' \
         {-C,--compress}'[Compress vmlinuz with lz4 or lzma.]:compression:(none lz4 lzma)' \
-        {-n,--name}'[Description of vmlinuz to put in the FIT.]:description: ' \
+        {-n,--name}'[Description of vmlinuz to put in the FIT.]:description:($(source /etc/os-release; echo "$NAME"))' \
         '*'{-c,--cmdline}'[Command-line parameters for the kernel.]:kernel cmdline:{_mkdepthcharge__cmdline}' \
         --no-kern-guid'[Do not prepend kern_guid=%U to the cmdline.]' \
         --bootloader'[Bootloader stub binary to use.]:bootloader file:_files' \
-        --devkeys'[Directory containing developer keys to use.]:devkeys dir:_directories' \
+        --keydir'[Directory containing vboot keys to use.]:keys dir:_directories' \
         --keyblock'[The key block file (.keyblock).]:keyblock file:_files' \
         --signprivate'[Private key (.vbprivk) to sign the image.]:vbprivk file:_files' \
+        --signpubkey'[Public key (.vbpubk) to verify the image.]:vbpubk file:_files' \
         ':vmlinuz file:_files' \
         '*:initrd or dtb files:_files' \
         ;
