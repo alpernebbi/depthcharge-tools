c89ef060138386fb1090366d7c401c8bbdf77c26
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2021-05-06 13:02:25 +0300

    setup.py: Bump version to 0.5.0
    I've tested and fixed as many things as I can on my chromebook,
    hopefully this is good enough for an ordinary release.
    
    Signed-off-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ readme = (root / 'README.rst').read_text()
 
 setuptools.setup(
     name='depthcharge-tools',
-    version='0.5.0.dev2',
+    version='0.5.0',
     description='Tools to manage the Chrome OS bootloader',
     long_description=readme,
     long_description_content_type="text/x-rst",
