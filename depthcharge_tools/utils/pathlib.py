94619fe87adb5fc2627a4417c227667898fc9305
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2022-09-28 23:43:12 +0300

    utils: pathlib: Add a decompress helper function
    Most often than not we care about decompressing the data, and not really
    about what algorithm it is compressed with. Add a function that tries
    all the compression programs we know, and returns whatever decompressed
    data it can.
    
    Signed-off-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>

--- depthcharge_tools/utils/pathlib.py
+++ depthcharge_tools/utils/pathlib.py
@@ -1,13 +1,42 @@
 #! /usr/bin/env python3
 
 import shutil
+import subprocess
 
 from pathlib import Path
 
+from depthcharge_tools.utils.subprocess import (
+    gzip,
+    lz4,
+    lzma,
+    lzop,
+    bzip2,
+    xz,
+    zstd,
+)
 
 def copy(src, dest):
     dest = shutil.copy2(src, dest)
     return Path(dest)
+
+
+def decompress(src, dest=None, partial=False):
+    if dest is not None:
+        dest = Path(dest)
+
+    for runner in (gzip, zstd, xz, lz4, lzma, bzip2, lzop):
+        try:
+            return runner.decompress(src, dest)
+
+        except subprocess.CalledProcessError as err:
+            if dest is None and err.output and partial:
+                return err.output
+
+            elif dest and dest.stat().st_size > 0 and partial:
+                return dest
+
+            elif dest:
+                dest.unlink()
 
 
 def iterdir(path):
