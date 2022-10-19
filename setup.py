ca8f53381deff5d432dfcf59bcff50ca55957fe8
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2022-10-19 09:45:22 +0300

    setup.py: Bump version to 0.6.0
    I hope this much is enough as the essentials. I can always generate more
    ideas on how I can make it better, but I already spent too much time on
    this much. Have to get back to other projects.
    
    Signed-off-by: Alper Nebi Yasak <alpernebiyasak@gmail.com>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ readme = (root / 'README.rst').read_text()
 
 setuptools.setup(
     name='depthcharge-tools',
-    version='0.5.1.dev0',
+    version='0.6.0',
     description='Tools to manage the Chrome OS bootloader',
     long_description=readme,
     long_description_content_type="text/x-rst",
